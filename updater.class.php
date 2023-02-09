b60604782f5021bd965148a72dd347efe77ed283
Alexandr Yachmenev <a_yachmenev@uacoders.com>
2023-02-09 23:42:19 +0200

    Issue-37 - Implement GitHub Updater mechanism. - add a plugin info

--- updater.class.php
+++ updater.class.php
@@ -14,11 +14,14 @@ class updater {
 
     public function __construct($file) {
         $this->file = $file;
-        add_action('admin_init', [$this, 'set_plugin_properties']);
+        $this->set_plugin_properties();
         return $this;
     }
 
     public function set_plugin_properties() {
+        // Ensure get_plugins() function is available.
+        include_once ABSPATH . 'wp-admin/includes/plugin.php';
+
         $this->plugin = get_plugin_data($this->file);
         $this->basename = plugin_basename($this->file);
         $this->active = is_plugin_active($this->basename);
@@ -92,39 +95,70 @@ class updater {
         }
     }
 
+    private function get_readme_info() {
+        $repositoryUrl = $this->plugin['UpdateURI'];
+        $path = parse_url($repositoryUrl, PHP_URL_PATH);
+        if ( preg_match('@^/?(?P<username>[^/]+?)/(?P<repository>[^/#?&]+?)/?$@', $path, $matches) ) {
+            $this->username = $matches['username'];
+            $this->repository = $matches['repository'];
+            $request_uri = sprintf('https://raw.githubusercontent.com%s/main/README.md', $path);
+            $args = [
+                'sslverify' => false,
+                'timeout'     => 0,
+                'redirection' => 10,
+                'httpversion' => '1.0',
+                'headers'     => array(
+                    'Authorization' => 'token ' . $this->authorize_token,
+                    'User-Agent' => 'woocoo/updater/0.1'
+                ),
+            ];
+            $response = wp_remote_get($request_uri, $args);
+            if ($response) {
+                return wp_remote_retrieve_body($response);
+            }
+        } else {
+            throw new InvalidArgumentException('Invalid GitHub repository URL: "' . $repositoryUrl . '"');
+        }
+        return false;
+    }
+
     public function initialize() {
-        add_filter('pre_set_site_transient_update_plugins', [$this, 'modify_transient'], 10, 1);
-        add_filter( 'plugin_icon', [ $this, 'plugin_icon' ] );
-        add_filter('plugins_api', [$this, 'plugin_popup'], 10, 3);
+        add_filter('site_transient_update_plugins', [$this, 'update_site_transient'], 15, 1);
+        add_filter('plugins_api', [$this, 'plugins_api'], 99, 3);
         add_filter('upgrader_post_install', [$this, 'after_install'], 10, 3);
     }
 
-    public function plugin_icon() {
-        return PLUGIN_URL . '/templates/assets/images/icon.svg';
-    }
+    public function update_site_transient( $transient ) {
+        // needed to fix PHP 7.4 warning.
+        if ( ! \is_object( $transient ) ) {
+            $transient = new \stdClass();
+        }
 
-    public function modify_transient($transient) {
         if (property_exists($transient, 'checked')) {
             if ($checked = $transient->checked) {
                 $this->get_repository_info();
 
+                $slug = current(explode('/', $this->basename));
+                $new_files = $this->github_response['zipball_url'];
+
+                $plugin = [
+                    'url' => $this->plugin['PluginURI'],
+                    'icons' => [
+                        'svg' => PLUGIN_URL . 'templates/assets/images/icon.svg',
+                    ],
+                    'slug' => $slug,
+                    'package' => $new_files,
+                    'new_version' => $this->github_response['tag_name']
+                ];
+
                 $out_of_date = version_compare($this->github_response['tag_name'], $checked[$this->basename], 'gt');
-
                 if ($out_of_date) {
-                    $new_files = $this->github_response['zipball_url'];
-                    $slug = current(explode('/', $this->basename));
-
-                    $plugin = [
-                        'url' => $this->plugin['PluginURI'],
-                        'icons' => [
-                            'svg' => PLUGIN_URL . 'templates/assets/images/icon.svg',
-                        ],
-                        'slug' => $slug,
-                        'package' => $new_files,
-                        'new_version' => $this->github_response['tag_name']
-                    ];
-
                     $transient->response[$this->basename] = (object) $plugin;
+                } else {
+                    // Add repo without update to $transient->no_update for 'View details' link.
+                    if ( ! isset( $transient->no_update[ $this->basename ] ) ) {
+                        $transient->no_update[ $this->basename ] = (object) $plugin;
+                    }
                 }
             }
         }
@@ -132,26 +166,29 @@ class updater {
         return $transient;
     }
 
-    public function plugin_popup($result, $action, $args) {
-        if ($action !== 'plugin_information') {
-            return false;
+    public function plugins_api( $false, $action, $response ) {
+        if ( ! ( 'plugin_information' === $action ) ) {
+            return $false;
         }
 
-        if (!empty($args->slug)) {
-            if ($args->slug == current(explode('/' , $this->basename))) {
+        if (!empty($response->slug)) {
+            if ($response->slug == current(explode('/' , $this->basename))) {
                 $this->get_repository_info();
 
+                $readme = $this->get_readme_info();
+
                 $plugin = [
+                    'slug' => $response->slug,
+                    'plugin_name' => $this->plugin['Name'],
                     'name' => $this->plugin['Name'],
-                    'slug' => $this->basename,
                     'version' => $this->github_response['tag_name'],
                     'author' => $this->plugin['AuthorName'],
                     'author_profile' => $this->plugin['AuthorURI'],
                     'last_updated' => $this->github_response['published_at'],
                     'homepage' => $this->plugin['PluginURI'],
-                    'short_description' => $this->plugin['Description'],
+                    'short_description' => substr( strip_tags( trim( $this->plugin['Description'] ) ), 0, 175 ) . '...',
                     'sections' => [
-                        'Description' => $this->plugin['Description'],
+                        'Description' => ($readme)?:$this->plugin['Description'],
                         'Updates' => $this->github_response['body'],
                     ],
                     'icons' => [
@@ -163,12 +200,11 @@ class updater {
                     ],
                     'download_link' => $this->github_response['zipball_url']
                 ];
-
                 return (object) $plugin;
             }
         }
 
-        return $result;
+        return $false;
     }
 
     public function after_install($response, $hook_extra, $result) {
