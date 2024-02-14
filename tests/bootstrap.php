bd41234a148f47a70307689fb5db363d69c558e5
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2024-02-14 03:34:59 +0100

    Moving test error supressors into a neater directory location

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,8 +5,8 @@ declare(strict_types = 1);
 // As WordPress is still using and supporting PHPUnit 6, we need to create
 // certain empty classes expected for that version of PHPUnit in order to
 // supress errors when we run PHPUnit 9.
-require './tests/SupressFrameworkError.php';
-require './tests/SupressFramework.php';
+require dirname( __FILE__ ) . '/_/SupressFrameworkError.php';
+require dirname( __FILE__ ) . '/_/SupressFramework.php';
 
 // Include the WordPress test suite
 //
