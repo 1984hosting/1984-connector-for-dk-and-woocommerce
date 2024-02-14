8a6036223a062bf1409cc778d6e47cbcf5428106
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2024-02-14 04:26:34 +0100

    Fixing PHPCS checks for the test suite

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,12 +5,9 @@ declare(strict_types = 1);
 // As WordPress is still using and supporting PHPUnit 6, we need to create
 // certain empty classes expected for that version of PHPUnit in order to
 // supress errors when we run PHPUnit 9.
-require dirname( __FILE__ ) . '/_/SupressFrameworkError.php';
-require dirname( __FILE__ ) . '/_/SupressFramework.php';
+require __DIR__ . '/_/SupressFrameworkError.php';
+require __DIR__ . '/_/SupressFramework.php';
 
-// Include the WordPress test suite
-//
-// Remember to run the following in order to get the test suite running:
-// `$ bin/install-wp-tests.sh wordpress-test root password 127.0.0.1 latest`
+// Include the WordPress test suite.
 require '/tmp/wordpress-tests-lib/includes/functions.php';
 require '/tmp/wordpress-tests-lib/includes/bootstrap.php';
