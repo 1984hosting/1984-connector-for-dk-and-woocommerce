67226617e8ad3909832a1611fff32318a8157361
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2024-03-26 15:59:46 +0100

    Documenting and typing the ShippingSKU class

--- src/Export/ShippingSKU.php
+++ src/Export/ShippingSKU.php
@@ -7,8 +7,24 @@ namespace NineteenEightyFour\NineteenEightyWoo\Export;
 use NineteenEightyFour\NineteenEightyWoo\Service\DKApiRequest;
 use NineteenEightyFour\NineteenEightyWoo\Config;
 
+/**
+ * The Shipping SKU class
+ *
+ * Provides functions for creating a product in DK representing a specific
+ * service item product used for shipping costs and fees.
+ */
 class ShippingSKU {
-	public static function is_in_dk( string $sku ) {
+	/**
+	 * Check if shipping SKU is in DK
+	 *
+	 * Checks the DK API if a product with a shipping SKU has been created and
+	 * saves that information in the options table.
+	 *
+	 * @param string $sku The SKU.
+	 *
+	 * @see NineteenEightyFour\NineteenEightyWoo\Config::set_shipping_sku_is_in_dk()
+	 */
+	public static function is_in_dk( string $sku ): bool {
 		$api_request = new DKApiRequest();
 
 		$result = $api_request->get_result(
@@ -24,7 +40,15 @@ class ShippingSKU {
 		return true;
 	}
 
-	public static function create_in_dk( string $sku ) {
+	/**
+	 * Create the shipping product in DK
+	 *
+	 * This creates a Service Item product in DK used for shipping that has a
+	 * specific SKU.
+	 *
+	 * @param string $sku The SKU.
+	 */
+	public static function create_in_dk( string $sku ): bool {
 		$api_request = new DKApiRequest();
 
 		$result = $api_request->request_result(
@@ -41,7 +65,15 @@ class ShippingSKU {
 		return true;
 	}
 
-	public static function post_body_for_dk( string $sku ) {
+	/**
+	 * Get POST body for creating the shipping product
+	 *
+	 * Creates an array representing a valid HTTP POST body for creating the
+	 * Shipping SKU.
+	 *
+	 * @param string $sku The SKU.
+	 */
+	public static function post_body_for_dk( string $sku ): array {
 		return array(
 			'ItemCode'          => $sku,
 			'ShowItemInWebShop' => false,
