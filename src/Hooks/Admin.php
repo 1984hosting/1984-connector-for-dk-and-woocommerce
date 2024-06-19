db39102c597252d7b65ccafce9e1e626d2367fbd
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2024-06-19 11:43:20 +0200

    Setting version to 0.2.3

--- src/Hooks/Admin.php
+++ src/Hooks/Admin.php
@@ -11,7 +11,7 @@ namespace NineteenEightyFour\NineteenEightyWoo\Hooks;
  * enqueues scripts and stylesheets etc.
  */
 class Admin {
-	const ASSET_VERSION = '0.2.1';
+	const ASSET_VERSION = '0.2.3';
 
 	/**
 	 * Constructor for the Admin interface class
