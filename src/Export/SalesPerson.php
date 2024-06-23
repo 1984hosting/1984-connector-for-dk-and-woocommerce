74ae971d3d802ada023c89f040458c02bb134d28
Alda Vigdís Skarphéðinsdóttir <aldavigdis@aldavigdis.is>
2024-06-23 22:32:31 +0200

    Documenting the sales person class

--- src/Export/SalesPerson.php
+++ src/Export/SalesPerson.php
@@ -7,9 +7,17 @@ namespace NineteenEightyFour\NineteenEightyWoo\Export;
 use NineteenEightyFour\NineteenEightyWoo\Service\DKApiRequest;
 use WP_Error;
 
+/**
+ * The sales person class
+ */
 class SalesPerson {
 	const API_PATH = '/sales/person/';
 
+	/**
+	 * Check if a sales person is in DK
+	 *
+	 * @param string $sales_person_number The intended sales person number. Note that there are case insensitivity issues here.
+	 */
 	public static function is_in_dk(
 		string $sales_person_number
 	): bool|WP_Error {
